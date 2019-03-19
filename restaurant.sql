e4e745dbe94926fadaebc90b1fda1948ac8d83c4
pranto <mahpranto@outlook.com>
2019-03-19 12:06:34 +0600

    Main

--- restaurant.sql
+++ restaurant.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2019 at 09:50 PM
+-- Generation Time: Mar 19, 2019 at 07:03 AM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.2.5
 
@@ -39,27 +39,29 @@ CREATE TABLE `addrating` (
 --
 
 INSERT INTO `addrating` (`id`, `resid`, `rating`) VALUES
-(5, 3, 1),
-(6, 3, 5),
-(7, 3, 4),
-(8, 3, 3),
-(9, 3, 2),
-(10, 3, 1),
-(11, 4, 2),
-(12, 4, 5),
-(13, 4, 1),
-(14, 4, 3),
-(15, 3, 5),
-(16, 3, 1),
-(17, 4, 5),
-(18, 5, 1),
-(19, 5, 5),
-(20, 4, 5),
-(21, 4, 5),
-(22, 5, 5),
-(23, 3, 5),
-(24, 4, 5),
-(25, 4, 5);
+(38, 7, 5),
+(39, 9, 4),
+(40, 8, 3),
+(41, 13, 5),
+(42, 11, 4),
+(43, 11, 5),
+(44, 10, 3),
+(45, 14, 5),
+(46, 15, 3),
+(47, 16, 1),
+(48, 17, 2),
+(49, 13, 2),
+(50, 14, 4),
+(51, 16, 5),
+(52, 15, 5),
+(53, 17, 5),
+(54, 15, 1),
+(55, 16, 5),
+(56, 16, 1),
+(57, 14, 5),
+(58, 17, 5),
+(59, 13, 5),
+(60, 13, 1);
 
 -- --------------------------------------------------------
 
@@ -73,17 +75,24 @@ CREATE TABLE `addrestaurant` (
   `resname` varchar(30) NOT NULL,
   `resadd` varchar(30) NOT NULL,
   `rescity` varchar(30) NOT NULL,
-  `restag` varchar(30) DEFAULT NULL
+  `restag` varchar(30) DEFAULT NULL,
+  `items` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `addrestaurant`
 --
 
-INSERT INTO `addrestaurant` (`id`, `username`, `resname`, `resadd`, `rescity`, `restag`) VALUES
-(3, 'zamil', 'De Alpanio', 'Bashundhara Residential Area', 'Dhaka', 'Quality is our best priority '),
-(4, 'kamal', 'Ninety Nine', 'Bashundhara Residential Area', 'Dhaka', 'Quality food'),
-(5, 'rakib', 'La Italiano', 'Bashundhara Residential Area', 'Dhaka', 'Best food');
+INSERT INTO `addrestaurant` (`id`, `username`, `resname`, `resadd`, `rescity`, `restag`, `items`) VALUES
+(8, 'kamal', 'Ninety Nine', 'Bashundhara Residential Area', 'Dhaka', 'Quality is our best priority ', 'rice,beef'),
+(9, 'helal', 'La Italiano', 'Kin Bridge', 'Sylhet', 'Only eat', 'steak,fry'),
+(10, 'ramiz', 'Cafe Zero', 'Bashundhara Residential Area', 'Dhaka', 'Best Cafe', 'rares,hsdf'),
+(11, 'faiza', 'Ola Herase', 'Bashundhara Residential Area', 'Dhaka', 'Different taste', 'Coffe,capachino,alabeno'),
+(13, 'zamil', 'De Alpanio', 'CK Ghosh Road', 'Mymensingh', 'Try new thing', 'Coffe,capachino,istamena'),
+(14, 'rashed', 'Rashed Food', 'Dhanmondi', 'Dhaka', 'Best is Best', 'rice,chicken'),
+(15, 'kader', 'Meal Deal', 'Santinogor', 'Dhaka', 'Khaooooo', 'rice,chicken,vegetable'),
+(16, 'hamid', 'Khana Pina', 'Malibag', 'Dhaka', 'Best food serve always', 'rice,chicken,vegetable,fish'),
+(17, 'zashim', 'Bela Food', 'Bashundhara Residential Area', 'Dhaka', 'Best food ever', 'singara,puri,shamucha');
 
 -- --------------------------------------------------------
 
@@ -103,9 +112,49 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `resid`, `username`, `comment`) VALUES
-(1, 3, 'pranto', 'This i a good restaurant.'),
-(2, 5, 'pranto', 'One of the best.'),
-(3, 4, 'pranto', 'Much better than other in town.');
+(10, 7, 'pranto', 'GOOD'),
+(11, 8, 'pranto', 'One of the best restaurant ever'),
+(12, 9, 'oli', 'goo food'),
+(13, 8, 'oli', 'good food'),
+(14, 13, 'pranto', 'yes'),
+(15, 11, 'pranto', ' nbfg'),
+(16, 16, 'pranto', 'good food'),
+(17, 17, 'pranto', 'good food'),
+(18, 16, 'pranto', 'Best Food'),
+(19, 17, 'pranto', 'good'),
+(20, 13, 'pranto', 'go'),
+(21, 13, 'pranto', 'ji'),
+(22, 14, 'pranto', 'good');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profileimage`
+--
+
+CREATE TABLE `profileimage` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `level` int(11) NOT NULL,
+  `name` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `profileimage`
+--
+
+INSERT INTO `profileimage` (`id`, `username`, `level`, `name`) VALUES
+(2, 'pranto', 1, '5987e6d51d24ac8a051140a5fb6e4c14.jpg'),
+(5, 'oli', 1, '13f4dee99b7e3946e6aca3ba3a0ce8c8.jpg'),
+(10, 'zamil', 2, 'a3e1b0b9388473b2ac411f5d620b3d7b.jpg'),
+(11, 'kamal', 2, '105ecc2b42c5d5fa5b0b0fd35c24bcf4.jpg'),
+(12, 'helal', 2, '39184a14f9795496a476411d83b6ed8a.jpg'),
+(13, 'ramiz', 2, NULL),
+(14, 'faiza', 2, NULL),
+(15, 'rashed', 2, '75a3f728faa050d57a5803b85c55b8ae.jpg'),
+(16, 'kader', 2, NULL),
+(17, 'hamid', 2, NULL),
+(18, 'zashim', 2, NULL);
 
 -- --------------------------------------------------------
 
@@ -124,9 +173,15 @@ CREATE TABLE `restaurantimage` (
 --
 
 INSERT INTO `restaurantimage` (`id`, `username`, `name`) VALUES
-(2, 'zamil', '7a78acf88794a1c1c80b829279fbf228.jpg'),
-(3, 'kamal', 'd4737ab7dfa488d20d185088e085e4ce.jpg'),
-(4, 'rakib', 'c308aa8b82be749fe12dc9b63612c087.jpg');
+(7, 'kamal', 'cedbfc4d16ffb19903f20d5679e83d79.jpg'),
+(8, 'helal', 'd2a102bc085f831fd71e8c68ab912e59.jpg'),
+(9, 'ramiz', '2ce378b46364eeb55b32d1d8c0086423.jpg'),
+(10, 'faiza', '2b7d4030e000d4e0a4ad5316286cdc81.jpg'),
+(12, 'zamil', '4398f1a938c61e8ca19810c84d282c8d.jpg'),
+(13, 'rashed', 'f64c5904566cbde517a28b28ac0be7db.jpg'),
+(14, 'kader', '2d87b8cc748dbc99b8f0eb52f079b0f5.jpg'),
+(15, 'hamid', 'df9f496655009aa210a7b36dc54284e7.jpg'),
+(16, 'zashim', 'ccf06c4c2939b3530422233ae88b3080.jpg');
 
 -- --------------------------------------------------------
 
@@ -149,11 +204,17 @@ CREATE TABLE `signup` (
 --
 
 INSERT INTO `signup` (`id`, `firstname`, `lastname`, `username`, `email`, `password`, `level`) VALUES
-(1, 'Md. Abu', 'Pranto', 'pranto', 'mahpranto@outlook.com', 'pranto', 1),
-(2, 'Zamil', 'Hassan', 'zamil', 'zamil@gmail.com', 'zamil', 2),
-(3, 'Kamal', 'Ahmed', 'kamal', 'kamal@gmail.com', 'kamal', 2),
-(4, 'Rakib', 'Hossain', 'rakib', 'rakib@gmail.com', 'rakib', 2),
-(5, 'Shakib', 'Rahman', 'shakib', 'shakib@gmail.com', 'shakib', 1);
+(1, 'Md. Abu Horyra', 'Pranto', 'pranto', 'mahpranto@outlook.com', 'pranto', 1),
+(7, 'Oli', 'Ullah', 'oli', 'oli@gmail.com', 'olo', 1),
+(8, 'Zamil', 'Hossain', 'zamil', 'zamil@outlook.com', 'ZAMIL', 2),
+(9, 'Kamal', 'Ahmed', 'kamal', 'kamal@gmail.com', 'kamal', 2),
+(10, 'Khan', 'Helal', 'helal', 'helal@gmail.com', 'helal', 2),
+(11, 'Ramiz', 'reza', 'ramiz', 'ramiz@gmail.com', 'ramiz', 2),
+(12, 'Fahmida', 'Faiza', 'faiza', 'faiza@gmail.com', 'faiza', 2),
+(13, 'Rashed', 'Khan', 'rashed', 'rashed@gmail.com', 'rashed', 2),
+(14, 'Kader', 'Hamid', 'kader', 'kader@gmail.com', 'kader', 2),
+(15, 'Hamid', 'Uddin', 'hamid', 'hamid@gmail.com', 'hamid', 2),
+(16, 'Zashim', 'Sheikh', 'zashim', 'zashim@gmail.com', 'zashim', 2);
 
 --
 -- Indexes for dumped tables
@@ -179,6 +240,14 @@ ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `profileimage`
+--
+ALTER TABLE `profileimage`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `username_2` (`username`);
+
+--
 -- Indexes for table `restaurantimage`
 --
 ALTER TABLE `restaurantimage`
@@ -202,31 +271,37 @@ ALTER TABLE `signup`
 -- AUTO_INCREMENT for table `addrating`
 --
 ALTER TABLE `addrating`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT for table `addrestaurant`
 --
 ALTER TABLE `addrestaurant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `profileimage`
+--
+ALTER TABLE `profileimage`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `restaurantimage`
 --
 ALTER TABLE `restaurantimage`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `signup`
 --
 ALTER TABLE `signup`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
